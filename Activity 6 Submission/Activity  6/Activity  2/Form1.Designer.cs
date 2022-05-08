1a57e53429b620542bac3fe6a52c8111771a0213
Naralla <104328391+Naralla@users.noreply.github.com>
2022-05-07 22:40:36 -0700

    Add files via upload

---
+++ Activity 6 Submission/Activity  6/Activity  2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Activity__2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.earthWeightBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.marsWeightBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your weight on Earth";
+            // 
+            // earthWeightBox
+            // 
+            this.earthWeightBox.Location = new System.Drawing.Point(248, 40);
+            this.earthWeightBox.Name = "earthWeightBox";
+            this.earthWeightBox.Size = new System.Drawing.Size(100, 20);
+            this.earthWeightBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Weight on Mars";
+            // 
+            // marsWeightBox
+            // 
+            this.marsWeightBox.Location = new System.Drawing.Point(248, 94);
+            this.marsWeightBox.Name = "marsWeightBox";
+            this.marsWeightBox.Size = new System.Drawing.Size(100, 20);
+            this.marsWeightBox.TabIndex = 2;
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.convertButton.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(40, 170);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(98, 43);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.exitButton.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(248, 170);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 43);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(396, 241);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.marsWeightBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.earthWeightBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Your Weight on Mars";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox earthWeightBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox marsWeightBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Activity 6 Submission/Activity  6/Activity  2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Dalton Danz
+//CST-150
+//May 8, 2022
+//This is my own work.
+
+namespace Activity__2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            decimal earthWeight = 0;
+            decimal marsWeight = 0;
+            decimal earthGravity = 9.81m;
+            decimal marsGravity = 3.711m;
+
+            try 
+                {
+                //get user input
+                    if (decimal.TryParse(earthWeightBox.Text, out earthWeight))
+                    {
+
+                        //process user input
+                        marsWeight = (earthWeight / earthGravity) * marsGravity;
+
+                        //return answer
+                        marsWeightBox.Text = marsWeight.ToString("N3");
+                    }                   
+                    //inner exception handler
+                    else
+                    {
+                        MessageBox.Show("Please enter a numeric weight value.");
+                    }
+                }
+
+
+            //exception handler
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
