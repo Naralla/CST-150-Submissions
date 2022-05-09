7d110286f9c60b20d6059897ee0d9214f30c01ca
Naralla <104328391+Naralla@users.noreply.github.com>
2022-05-08 17:04:54 -0700

    Add files via upload

---
+++ Activity 5 Submission/Activity 5/Activity 5/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Activity_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.processButton = new System.Windows.Forms.Button();
+            this.resultsBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please select a file for processing";
+            // 
+            // processButton
+            // 
+            this.processButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.processButton.FlatAppearance.BorderSize = 2;
+            this.processButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.processButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.processButton.Location = new System.Drawing.Point(296, 46);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(110, 53);
+            this.processButton.TabIndex = 2;
+            this.processButton.Text = "Select file for processing";
+            this.processButton.UseVisualStyleBackColor = false;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.Location = new System.Drawing.Point(12, 161);
+            this.resultsBox.Multiline = true;
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.Size = new System.Drawing.Size(432, 93);
+            this.resultsBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "The results of processing are as follows,";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Title = "File for processing";
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.exitButton.FlatAppearance.BorderSize = 2;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(431, 47);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(110, 53);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(553, 280);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "File Processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.TextBox resultsBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
