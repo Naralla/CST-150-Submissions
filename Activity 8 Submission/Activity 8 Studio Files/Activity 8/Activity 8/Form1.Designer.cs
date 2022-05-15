e189019c29db3d34bf531c2650e62971c3daf43f
Naralla <104328391+Naralla@users.noreply.github.com>
2022-05-15 10:00:31 -0700

    Add files via upload

---
+++ Activity 8 Submission/Activity 8 Studio Files/Activity 8/Activity 8/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Activity_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gramsOfFatBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gramsOfCarbsBox = new System.Windows.Forms.TextBox();
+            this.resultsBox = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Grams of Fat";
+            // 
+            // gramsOfFatBox
+            // 
+            this.gramsOfFatBox.Location = new System.Drawing.Point(296, 41);
+            this.gramsOfFatBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gramsOfFatBox.Name = "gramsOfFatBox";
+            this.gramsOfFatBox.Size = new System.Drawing.Size(148, 26);
+            this.gramsOfFatBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Grams of Carbs";
+            // 
+            // gramsOfCarbsBox
+            // 
+            this.gramsOfCarbsBox.Location = new System.Drawing.Point(296, 123);
+            this.gramsOfCarbsBox.Name = "gramsOfCarbsBox";
+            this.gramsOfCarbsBox.Size = new System.Drawing.Size(148, 26);
+            this.gramsOfCarbsBox.TabIndex = 3;
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.Location = new System.Drawing.Point(90, 264);
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.Size = new System.Drawing.Size(343, 66);
+            this.resultsBox.TabIndex = 4;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.Color.Silver;
+            this.calculateButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.calculateButton.FlatAppearance.BorderSize = 2;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.calculateButton.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(296, 206);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(102, 44);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate Calories";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Silver;
+            this.exitButton.FlatAppearance.BorderSize = 2;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(431, 206);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(102, 44);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(545, 339);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.gramsOfCarbsBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gramsOfFatBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Calorie Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox gramsOfFatBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gramsOfCarbsBox;
+        private System.Windows.Forms.Label resultsBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Activity 8 Submission/Activity 8 Studio Files/Activity 8/Activity 8/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Dalton Danz
+//CST-150
+//May 15, 2022
+//This is my own work
+
+namespace Activity_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Fat Calories calculation method
+        private int FatCalories(int fatGrams)
+        {
+            return fatGrams * 9;
+        }
+
+        //Carbs Calories calculation method
+        private int CarbCalories(int carbsGrams)
+        {
+            return carbsGrams * 4;
+        }
+
+
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //variable declarations
+            int caloriesFromFat;
+            int caloriesFromCarbs;
+            int gramsFromFat;
+            int gramsFromCarbs;
+
+           //clear previous results
+            resultsBox.Text = "";
+
+            try
+            {
+                //for exception handling
+                if(int.TryParse(gramsOfFatBox.Text, out gramsFromFat))
+                {
+                    if(int.TryParse(gramsOfCarbsBox.Text, out gramsFromCarbs))
+                    {
+
+                        //method calls for calculations
+                        caloriesFromFat = FatCalories(gramsFromFat);
+                        caloriesFromCarbs = CarbCalories(gramsFromCarbs);
+
+                        //output results
+                        resultsBox.Text = ("The Calories from Fat are " + caloriesFromFat.ToString() + "and the Calories from Carbs are " 
+                            + caloriesFromCarbs.ToString());
+                    }
+
+                    //exception handling
+                    else
+                    {
+                        MessageBox.Show("Invalid Grams of Carbs. Please enter an integer value of Grams of Carbs");
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Grams of Fat. Please enter an integer value of Grams of Fat");
+                }
+                    
+            }
+            //error catching during building
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //exit button
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
