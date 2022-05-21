0c307d3a026e7ec0f65d9569eb5ac606250a3ed6
Naralla <104328391+Naralla@users.noreply.github.com>
2022-05-21 15:56:41 -0700

    Add files via upload

---
+++ Activity 11 Submission/Activity 11 Files/Activity 11/Activity 11/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Dalton Danz
+//CST-150
+//May 21, 2022
+//This is my own work
+
+namespace Activity_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rollDiceButton_Click(object sender, EventArgs e)
+        {
+            //generate random number of sides for dice. number of sides restricted to 
+            //meet constructor criteria
+            Random randNumOfSides = new Random();
+            int numOfSides = randNumOfSides.Next(4, 20);
+
+            //create dice objects
+            Dice die1 = new Dice(numOfSides, 0);
+            Dice die2 = new Dice(numOfSides, 0);
+
+            //initialize count number of rolls until snake eyes
+            int numOfRolls = 0;
+
+            do
+            {
+                //roll both dice
+                die1.RollDie();
+                die2.RollDie();
+
+                //increment number of rolls
+                numOfRolls++;
+
+                //keep rolling until snake eyes
+            } while (die1.isSnakeEyes(die2));
+                    
+                //display results
+                MessageBox.Show("It took " + numOfRolls + " rolls to get snake eyes!");
+        }
+    }
+}
+
+
+//Dice class
+public class Dice
+{
+
+    //initialize variables
+    public int valueOfSide;
+    private int numberOfSides;
+    Random rollNumber = new Random();
+
+    //constructors
+    public Dice(int numberOfSides, int valueOfSide)
+    {
+        //to restrict constructor to intended values only
+        if(numberOfSides <= 20 && numberOfSides >= 4)
+        {
+            this.numberOfSides = numberOfSides;
+            this.valueOfSide = valueOfSide;
+        }
+        else
+        {
+            MessageBox.Show("Invalid number of sides entered. Please enter an integer value " +
+                "between 4 and 20");
+        }
+    }
+
+    public Dice()
+    {
+        this.numberOfSides = 0;
+        this.valueOfSide = 0;
+    }
+
+    //getters and setters
+    public int GetNumberOfSides()
+    {
+        return numberOfSides;
+    }
+    public int GetValueOfSide()
+    {
+        return valueOfSide;
+    }
+
+    public void SetNumberOfSides(int numberOfSides)
+    {
+        this.numberOfSides = numberOfSides;
+    }
+
+    //rolls the dice
+    public int RollDie()
+    {
+        this.valueOfSide = (int)(rollNumber.Next(1, this.numberOfSides));
+        return this.valueOfSide;
+    }
+
+    //checks for snake eyes
+    public bool isSnakeEyes(Dice rcvDice)
+    {
+        if (this.valueOfSide != 1 && rcvDice.valueOfSide != 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
