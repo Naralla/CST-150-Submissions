981a16d933c3e826aa66af674f4804f13e84f4cf
Naralla <104328391+Naralla@users.noreply.github.com>
2022-05-27 15:38:05 -0700

    Add files via upload

---
+++ Activity 13 Submission/Activity 13 Files/Activity 13/Activity 13/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.winnerLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(87, 552);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(158, 62);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(251, 552);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(158, 62);
+            this.exitButton.TabIndex = 17;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerLabel.Location = new System.Drawing.Point(57, 489);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(383, 53);
+            this.winnerLabel.TabIndex = 18;
+            this.winnerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 19;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(168, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 20;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(324, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 21;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 150);
+            this.button4.TabIndex = 22;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(168, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 150);
+            this.button5.TabIndex = 23;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(324, 177);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 150);
+            this.button6.TabIndex = 24;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(12, 333);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 150);
+            this.button7.TabIndex = 25;
+            this.button7.Text = " ";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(168, 333);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 150);
+            this.button8.TabIndex = 26;
+            this.button8.Text = " ";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(324, 333);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 150);
+            this.button9.TabIndex = 27;
+            this.button9.Text = " ";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(505, 626);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label winnerLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Activity 13 Submission/Activity 13 Files/Activity 13/Activity 13/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Dalton Danz
+//CST-150
+//May 27, 2022
+//I used source code from the following video to complete this assignment:
+//https://www.youtube.com/user/shadsluiter
+//This helped me to setup the game button clicking method and game grid array.
+
+namespace Activity_13
+{
+    public partial class Form1 : Form
+    {
+        //initialize classes
+        GameBoard game = new GameBoard();
+        Button[] gameButtons = new Button[9];
+
+        //tracks the turn of players
+        int playerTurn = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //assigns GUI buttons to array
+            gameButtons[0] = button1;
+            gameButtons[1] = button2;
+            gameButtons[2] = button3;
+            gameButtons[3] = button4;
+            gameButtons[4] = button5;
+            gameButtons[5] = button6;
+            gameButtons[6] = button7;
+            gameButtons[7] = button8;
+            gameButtons[8] = button9;
+
+            //assigns button with a click handler and locks in player choice
+            for (int i = 0; i < 9; i++)
+            {
+                gameButtons[i].Click += buttonIsClicked;
+                gameButtons[i].Tag = i;
+            }
+        }
+
+        //when a spot is chosen
+        private void buttonIsClicked(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            int playerSelection = (int)clickedButton.Tag;
+
+            //player 1 turn
+            if (playerTurn % 2 != 0)
+            {
+                game.gameGrid[playerSelection] = 1;
+
+                refreshTheBoard();
+            }
+            //player 2 turn
+            else
+            {
+                game.gameGrid[playerSelection] = 2;
+
+                refreshTheBoard();
+            }
+
+            //look for if move = win or tie
+            if (game.checkForWinner() == 1)
+            {
+                winnerLabel.Text = "Player 1 wins!";
+            }
+            if (game.checkForWinner() == 2)
+            {
+                winnerLabel.Text = "Player 2 wins!";
+            }
+            else if (game.boardFull() == true && game.checkForWinner() == 0)
+            {
+                winnerLabel.Text = "The Game is  Tie";
+            }
+
+            playerTurn++;
+        }
+
+
+        //shows the choices made on the board & resets for new game
+        private void refreshTheBoard()
+        {         
+            for (int i = 0; i < game.gameGrid.Length; i++)
+            {
+                if (game.gameGrid[i] == 0)
+                {
+                    gameButtons[i].Text = "";
+                    gameButtons[i].Enabled = true;
+                }
+                else if (game.gameGrid[i] == 1)
+                {
+                    gameButtons[i].Text = "X";
+                    gameButtons[i].Enabled = false;
+                }
+                else if (game.gameGrid[i] == 2)
+                {
+                    gameButtons[i].Text = "O";
+                    gameButtons[i].Enabled = false;
+                }
+            }
+        }
+
+        //new game button
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            //resets all items and text
+            game = new GameBoard();
+            winnerLabel.Text = "";
+            refreshTheBoard();
+            playerTurn = 1;
+        }
+
+        //exit button
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    //board class
+    class GameBoard
+    {
+
+        //initialize the grid
+        public int[] gameGrid = new int[9];
+
+
+        //constructor
+        public GameBoard()
+        {
+            //fills the board with empty choices
+            for (int i = 0; i < 9; i++)
+            {
+                gameGrid[i] = 0;
+            }
+        }
+
+        //checks for if the board is full
+        public bool boardFull()
+        {
+            bool boardFull = true;
+            for (int i = 0; i < gameGrid.Length; i++)
+            {
+                if (gameGrid[i] == 0)
+                {
+                    boardFull = false;
+                }
+            }
+            return boardFull;
+        }
+
+        //find a winner return 0 for no winner. return player number for winner
+        public int checkForWinner()
+        {
+            //top row
+            if (gameGrid[0] == gameGrid[1] && gameGrid[1] == gameGrid[2])
+            {
+                return gameGrid[0];
+            }
+
+            //middle row
+            if (gameGrid[3] == gameGrid[4] && gameGrid[4] == gameGrid[5])
+            {
+                return gameGrid[3];
+            }
+
+            //bottom row
+            if (gameGrid[6] == gameGrid[7] && gameGrid[7] == gameGrid[8])
+            {
+                return gameGrid[6];
+            }
+
+            //left column
+            if (gameGrid[0] == gameGrid[3] && gameGrid[3] == gameGrid[6])
+            {
+                return gameGrid[0];
+            }
+
+            //middle column
+            if (gameGrid[1] == gameGrid[4] && gameGrid[4] == gameGrid[7])
+            {
+                return gameGrid[1];
+            }
+
+            //right column
+            if (gameGrid[2] == gameGrid[5] && gameGrid[5] == gameGrid[8])
+            {
+                return gameGrid[2];
+            }
+
+            //top left to bottom right diagonal
+            if (gameGrid[0] == gameGrid[4] && gameGrid[4] == gameGrid[8])
+            {
+                return gameGrid[0];
+            }
+
+            //bottom left to top right diagonal
+            if (gameGrid[6] == gameGrid[4] && gameGrid[4] == gameGrid[2])
+            {
+                return gameGrid[6];
+            }
+
+            return 0;
+        }
+    }
+}
