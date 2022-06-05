61419049b8b87b847a99b45fc8971db0ea605fee
Naralla <104328391+Naralla@users.noreply.github.com>
2022-06-05 10:16:41 -0700

    Add files via upload

---
+++ Milestone 5, CST150/Milestone Files/Milestone Project/Milestone Project/Form2.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Milestone_Project
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inventoryListBox = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.closeWindowButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inventoryListBox
+            // 
+            this.inventoryListBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inventoryListBox.FormattingEnabled = true;
+            this.inventoryListBox.ItemHeight = 19;
+            this.inventoryListBox.Location = new System.Drawing.Point(48, 69);
+            this.inventoryListBox.Name = "inventoryListBox";
+            this.inventoryListBox.Size = new System.Drawing.Size(366, 251);
+            this.inventoryListBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Gainsboro;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(100, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(274, 33);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Inventory Update Log";
+            // 
+            // closeWindowButton
+            // 
+            this.closeWindowButton.BackColor = System.Drawing.Color.DimGray;
+            this.closeWindowButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeWindowButton.Location = new System.Drawing.Point(148, 347);
+            this.closeWindowButton.Name = "closeWindowButton";
+            this.closeWindowButton.Size = new System.Drawing.Size(169, 47);
+            this.closeWindowButton.TabIndex = 16;
+            this.closeWindowButton.Text = "Close this window";
+            this.closeWindowButton.UseVisualStyleBackColor = false;
+            this.closeWindowButton.Click += new System.EventHandler(this.closeWindowButton_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(476, 422);
+            this.Controls.Add(this.closeWindowButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.inventoryListBox);
+            this.Name = "Form2";
+            this.Text = "Inventory Overview";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox inventoryListBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button closeWindowButton;
+    }
+}
